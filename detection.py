1339a68625ab1d23ca5a52e183810556e20bffbb
aditi11133 <editguitar3@gmail.com>
2025-04-21 17:32:03 +0530

    initial commit

--- detection.py
+++ detection.py
@@ -1,6 +1,6 @@
 import cv2
 import cv2.data
-face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascades_frontalface_default.xml')
+face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 cap=cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Error:Could not open Camera")
@@ -14,9 +14,9 @@ while True:
 
     faces=face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5,minSize=(30,30))
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
     cv2.imshow('Face Detection- Press q to Quit',frame)
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
 cap.release()
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
--- filter.py
+++ filter.py
@@ -6,7 +6,7 @@ def apply_color_filter(image,filter_type):
     filtered_image=image.copy()
     if filter_type=="red_tint":
         filtered_image[:,:,1]=0
-        filtered_image[:,:,2]=0
+        filtered_image[:,:,0]=0
     elif filter_type=="blue_tint":
         filtered_image[:,:,1]=0
         filtered_image[:,:,2]=0
@@ -19,7 +19,7 @@ def apply_color_filter(image,filter_type):
         filtered_image[:,:,0]=cv2.subtract(filtered_image[:,:,0],50)
     return filtered_image
 
-image_path='image.jpg'
+image_path='dice.jpg'
 image=cv2.imread(image_path)
 if image is None:
     print("Image not found")
